5cbd62c9af296998960d2c019c27a464497eb3d4
GloomyMage <ysccm@hotmail.com>
2024-03-29 14:06:49 +0100

    Chase

--- Assets/Scripts/SCRPT_Chase.cs
+++ Assets/Scripts/SCRPT_Chase.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class SCRPT_Chase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public SpriteRenderer sprite_renderer;
+    public Transform[] patrolPoints;
+    public int patrolDestination = 0;
+    [SerializeField] float movespeed = 5f;
+    public Transform playerTransform;
+    public bool isChasing;
+    public float chaseDistance;
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemymove();
+    }
+
+    private void enemymove()
+    {
+        if (isChasing)
+        {
+            if (transform.position.x > playerTransform.position.x)
+            {
+                transform.position += Vector3.left * movespeed * Time.deltaTime;
+            }
+
+            if (transform.position.x < playerTransform.position.x)
+            {
+                transform.position += Vector3.right * movespeed * Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
+            {
+                isChasing = true;
+            }
+
+            if (patrolDestination == 0)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, movespeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
+                {
+                    Debug.Log("Hi");
+                    sprite_renderer.flipX = false;
+                    patrolDestination = 1;
+                }
+            }
+
+            if (patrolDestination == 1)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, movespeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
+                {
+                    sprite_renderer.flipX = true;
+                    patrolDestination = 0;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/SCRPT_PatrolBorder.cs
+++ Assets/Scripts/SCRPT_PatrolBorder.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SCRPT_PatrolBorder : MonoBehaviour
 {
+    public SpriteRenderer sprite_renderer;
     public Transform[] patrolPoints;
     public int patrolDestination = 0;
     [SerializeField] float movespeed = 5f;
@@ -11,12 +12,17 @@ public class SCRPT_PatrolBorder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        enemymove();
+    }
+
+    private void enemymove()
+    {
         if (patrolDestination == 0)
         {
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, movespeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
             {
-               // sprite_renderer.flipX = false;
+                sprite_renderer.flipX = true;
                 patrolDestination = 1;
             }
         }
@@ -24,9 +30,9 @@ public class SCRPT_PatrolBorder : MonoBehaviour
         if (patrolDestination == 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, movespeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
             {
-               // sprite_renderer.flipX = true;
+                sprite_renderer.flipX = false;
                 patrolDestination = 0;
             }
         }
